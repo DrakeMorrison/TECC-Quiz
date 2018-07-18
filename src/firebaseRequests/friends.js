59831ca32538f5168a1181853354ed3dbef3a036
Drake Morrison <drakermorrison@gmail.com>
2018-07-17 19:32:32 -0500

    Styled Menu a bit; made Get request for friends and games

--- src/firebaseRequests/friends.js
+++ src/firebaseRequests/friends.js
@@ -14,4 +14,24 @@ const postRequest = (friend) => {
   });
 };
 
-export default { postRequest };
+const getByUidRequest = (uid) => {
+  return new Promise((resolve, reject) => {
+    axios
+      .get(`${constants.firebaseConfig.databaseURL}/friends.json?orderBy="uid"&equalTo="${uid}"`)
+      .then(res => {
+        const stuff = [];
+        if (res.data !== null) {
+          Object.keys(res.data).forEach(fbKey => {
+            res.data[fbKey].id = fbKey;
+            stuff.push(res.data[fbKey]);
+          });
+        }
+        resolve(stuff);
+      })
+      .catch(err => {
+        reject(err);
+      });
+  });
+};
+
+export default { postRequest, getByUidRequest };
