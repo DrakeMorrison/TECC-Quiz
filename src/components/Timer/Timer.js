052be649f9704179d139ab9c23ae977363b878d5
Drake Morrison <drakermorrison@gmail.com>
2018-08-04 12:26:21 -0500

    removed comments

--- src/components/Timer/Timer.js
+++ src/components/Timer/Timer.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-// import Moment from 'moment';
-// import MomentCountdown from 'react-moment-countdown';
 import Countdown from 'react-countdown-now';
 
 import helper from '../../helpers';
@@ -14,7 +12,6 @@ class Timer extends React.Component {
 
   render () {
     const {startTime, gameOver} = this.props;
-    // const dateInFuture = Moment(startTime, 'x');
 
     return (
       <div className='Timer'>
@@ -22,17 +19,11 @@ class Timer extends React.Component {
           date={startTime}
           intervalDelay={0}
           precision={1}
-          renderer={props => <div id='timer-value' data-time={props.total}>{helper.formatTime(props.total)}</div>} // TODO: format time
+          renderer={props => <div id='timer-value' data-time={props.total}>{helper.formatTime(props.total)}</div>}
           onComplete={gameOver}
         >
           <div className='jumbotron'><h2>Game Over</h2></div>
         </Countdown>
-        {/* <MomentCountdown
-          toDate={dateInFuture}
-          onCountdownEnd={gameOver}
-          targetFormatMask='ss:ms'
-        >
-        </MomentCountdown> */}
       </div>
     );
   };
